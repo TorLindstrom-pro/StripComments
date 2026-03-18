6b463164366aa34bc3fb486bf46ab794c11a2448
TorLindstrom <tor.lindstrom@lasia.se>
2026-03-18 14:38:26 +0100

    feat: empty text reutns empty text

---
+++ ClassLibrary1.Tests/StripCommentsSolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using ClassLibrary1;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace ClassLibrary1.Tests;
+
+[TestSubject(typeof(StripCommentsSolution))]
+public class StripCommentsSolutionTest
+{
+	[Fact(DisplayName = "Empty text returns empty text")]
+	public async Task EmptyText_ReturnsEmptyText()
+	{
+		// Act
+		var result = StripCommentsSolution.StripComments("", new[] { "" });
+
+		// Assert
+		Assert.Equal("", result);
+	}
+}
---
+++ ClassLibrary1/StripCommentsSolution.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary1;
+
+public class StripCommentsSolution
+{
+	public static string StripComments(string text, string[] commentSymbols)
+	{
+		return "";
+	}
+}
